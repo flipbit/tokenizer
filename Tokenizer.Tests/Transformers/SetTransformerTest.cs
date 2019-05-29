fb9cfd375cb2b482035f24859ac3fe1fddac0813
Chris Wood <chris@flipbit.co.uk>
2019-05-29 05:48:30 +0100

    Add Set Transformer to set a token value
    - Set transformer sets a token value regardless of input content

---
+++ Tokenizer.Tests/Transformers/SetTransformerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tokens.Transformers
+{
+    [TestFixture]
+    public class SetTransformerTest
+    {
+        private SetTransformer transformer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            transformer = new SetTransformer();
+        }
+
+        [Test]
+        public void TestSet()
+        {
+            var result = transformer.Transform("input", "output");
+
+            Assert.AreEqual("output", result);
+        }
+
+        [Test]
+        public void TestSetWhenEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => transformer.Transform(string.Empty));
+        }
+
+        [Test]
+        public void TestSetWhenTooManyArguments()
+        {
+            Assert.Throws<ArgumentException>(() => transformer.Transform("input", "1", "2"));
+        }
+
+        [Test]
+        public void TestInTemplate()
+        {
+            var pattern = @"Name: { Name : Set('Alice') }";
+            var input = "Name: Bob";
+
+            var result = new Tokenizer().Tokenize(pattern, input);
+
+            Assert.AreEqual("Alice", result.Values["Name"]);
+        }
+    }
+}
---
+++ Tokenizer/Transformers/SetTransformer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tokens.Transformers
+{
+    /// <summary>
+    /// Sets the token value 
+    /// </summary>
+    public class SetTransformer : ITokenTransformer
+    {
+        public object Transform(object value, params string[] args)
+        {
+            if (args == null || args.Length != 1)
+            {
+                throw new ArgumentException("Set() must specified one argument to set - Set( value)");
+            }
+
+            return args[0];
+        }
+    }
+}

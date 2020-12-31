fe1b762a917687f06dbecaae6c9dd0bb9821ca53
AJ Henderson <ahenderson@datto.com>
2020-12-31 12:46:42 -0500

    Add support for loose URL handling and loose Absolute URL handling.

---
+++ Tokenizer/Validators/IsLooseUrlValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tokens.Validators
+{
+    /// <summary>
+    /// Validator to determine if a token value is a URL
+    /// </summary>
+    public class IsLooseUrlValidator : ITokenValidator
+    {
+        /// <summary>
+        /// Determines whether the specified token is valid.
+        /// </summary>
+        public bool IsValid(object value, params string[] args)
+        {
+            if (value == null) return false;
+
+            var valueString = value.ToString();
+
+            if (string.IsNullOrEmpty(valueString)) return false;
+
+            var result = Uri.IsWellFormedUriString(valueString, UriKind.RelativeOrAbsolute);
+
+            if (!result)
+			{
+                result = Uri.IsWellFormedUriString(string.Format("http://{0}", valueString), UriKind.RelativeOrAbsolute);
+			}
+
+            return result;
+        }
+    }
+}

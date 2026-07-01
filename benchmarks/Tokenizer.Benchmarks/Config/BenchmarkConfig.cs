33d74316e8f781ebaa249ba873fefd6a7d3429be
Chris Wood <chris@flipbit.co.uk>
2026-07-01 09:17:33 +0200

    Add benchmark project scaffolding with BenchmarkDotNet

---
+++ benchmarks/Tokenizer.Benchmarks/Config/BenchmarkConfig.cs
@@ -0,0 +1,17 @@
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+
+namespace Tokens.Config;
+
+public class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddDiagnoser(ThreadingDiagnoser.Default);
+        AddColumn(StatisticColumn.P95);
+        AddExporter(MarkdownExporter.GitHub);
+    }
+}

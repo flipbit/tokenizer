2799221e7920a19b1447a54bf7044fc43ff6654f
Chris Wood <chris@flipbit.co.uk>
2026-07-07 14:42:38 +0200

    style: fix MA0182 — remove dead HintProcessor, suppress polyfill false positive
    Also removes dead test dev utilities (AssertWriter, WindowsClipboard, SerilogConfig)
    that were triggering the same MA0182 violation.

--- src/Tokenizer/IsExternalInit.cs
+++ src/Tokenizer/IsExternalInit.cs
@@ -6,6 +6,8 @@ namespace System.Runtime.CompilerServices;
 /// <summary>
 /// Reserved for compiler use. This type enables the use of init accessors in netstandard2.0.
 /// </summary>
+#pragma warning disable MA0182 // Polyfill — used by compiler on netstandard2.0 only
 internal static class IsExternalInit;
+#pragma warning restore MA0182
 #pragma warning restore IDE0130
 #endif
